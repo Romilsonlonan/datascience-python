1ed85198c9fcaebe47fb4cff86c7cfde11a2c18b
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-14 20:12:22 -0300

    Soma de Matrizes

---
+++ Variáveis, tipos e estruturas de dados/Soma/somamatriz4.py
@@ -0,0 +1,14 @@
+# Soma de Matrizes
+
+import numpy as np
+
+A = np.array([[1,2,3],
+            [4,5,6]])
+
+B = np.array([[6,5,4],
+            [3,2,1]])
+
+# soma tradicional
+
+soma = A + B
+print(soma) 
