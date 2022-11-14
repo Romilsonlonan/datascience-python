f126be7e5aa1c187a3a76df3251c2e4e9d0ee9e1
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-14 19:58:27 -0300

    Criando uma Array em Python

---
+++ Variáveis, tipos e estruturas de dados/Array/array.py
@@ -0,0 +1,10 @@
+# Criando uma array
+
+import numpy as np
+
+array = np.array([1,2,3,4,5,6,7,8,9])
+
+type(array)
+
+matriz = np.arange(15).reshape(3,5)
+print(matriz)
--- Variáveis, tipos e estruturas de dados/subtracao/subtracao.py
+++ Variáveis, tipos e estruturas de dados/subtracao/subtracao.py
@@ -1,3 +1,5 @@
+#script que pede dois números, subtrai um do outro e exibe o resultado
+
 var1 = int( input("Digite um inteiro: ") )
 var2 = int( input("Digite outro inteiro: ") )
 
