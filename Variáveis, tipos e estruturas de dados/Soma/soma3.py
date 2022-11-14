159be0992eae4129d83318620970ac68c94e64c6
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-14 18:40:56 -0300

    Soma das notas com a função  len

---
+++ Variáveis, tipos e estruturas de dados/Soma/soma3.py
@@ -0,0 +1,7 @@
+#Soma em len
+# Len () é uma função integrada ao Python que é utilizada para obter o número de itens em um determinado objeto, string, array, listas, entre outros.
+
+notas = [0, 0, 9.0, 8.0, 5.0, 10.0, 7.0, 7.5, 4.0, 10.0, 7.0, 7.0, 8.0, 8.0, 7.5]
+
+qtd_de_notas = len(notas)
+print(qtd_de_notas)
