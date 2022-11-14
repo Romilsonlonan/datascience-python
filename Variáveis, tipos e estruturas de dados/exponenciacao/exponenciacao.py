56e8c01fa08fab9df8e8ca7cebed47b4f6cb249d
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-14 19:32:43 -0300

    Exponenciação em Python

---
+++ Variáveis, tipos e estruturas de dados/exponenciacao/exponenciacao.py
@@ -0,0 +1,13 @@
+# Exponenciação em Python: **
+
+# 3 ** 2 = 9, pois 3x3 =9
+
+# 3 elevado a 3:
+# 3 ** 3 = 27, pois 3x3x3=27
+
+var1 = int( input("Digite um inteiro: ") )
+var2 = int( input("Digite outro inteiro: ") )
+
+exp = var1 ** var2
+
+print(exp)
