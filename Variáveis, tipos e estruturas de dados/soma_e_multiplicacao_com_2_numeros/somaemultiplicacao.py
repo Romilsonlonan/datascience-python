31adcd8410ed42dab63d226f2426d7a928dc2321
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-15 00:06:57 -0300

    Soma e Multiplicação

---
+++ Variáveis, tipos e estruturas de dados/soma_e_multiplicacao_com_2_numeros/somaemultiplicacao.py
@@ -0,0 +1,9 @@
+# Criando um programa que recebe dois numeros inteiros e depois apresenta 
+# o resultados da soma e da multiplicação, de um numero pelo outro.
+
+numero1 = int(input("informe um número: "))
+numero2 = int(input("informe outro numero: "))
+
+resultado_soma = numero1 + numero2
+resultado_multiplicacao = numero1 * numero2
+print("A soma foi: ", resultado_soma, "e a multiplicação foi", resultado_multiplicacao)
