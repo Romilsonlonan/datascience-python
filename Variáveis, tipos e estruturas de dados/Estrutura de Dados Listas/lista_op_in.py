a5c0be62b037b12075fc55127531fdd7b21a63c1
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-16 18:06:58 -0300

    Lista operador in com retorno true

--- Variáveis, tipos e estruturas de dados/Estrutura de Dados Listas/lista_op_in.py
+++ Variáveis, tipos e estruturas de dados/Estrutura de Dados Listas/lista_op_in.py
@@ -2,3 +2,12 @@
 
 lista_teste_op = [100, 2, -5, 3.4]
 print(10 in lista_teste_op)
+
+lista_teste_op = [100, 2, -5, 3.4]
+print(100 in lista_teste_op)
+
+
+
+
+
+
---
+++ Variáveis, tipos e estruturas de dados/Estrutura de Dados Listas/lista_op_in1.py
@@ -0,0 +1,4 @@
+# Lista oprador in com retorno true
+
+lista_teste_op = [100, 2, -5, 3.4]
+print(100 in lista_teste_op)
