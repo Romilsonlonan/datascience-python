d01182cb54d015e926169a032e229553ed0bb4d5
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-16 19:39:20 -0300

    Criando uma lista estados com o operador reverse

---
+++ Variáveis, tipos e estruturas de dados/Estrutura de Dados Listas/lista_reverse_estados.py
@@ -0,0 +1,14 @@
+# Criando uma lista estados com o operador insert 
+
+"""
+o método insert (list insert python) insere um item numa 
+posição específica dentro da lista. Na hora de colocar os 
+argumentos você coloca o número do índice que o item será 
+inserido e em seguida o valor a ser adicionado.
+"""
+
+estados = ["Brasília", "São Paulo", "Rio de Janeiro", "Minas-Gerais" ]
+estados.extend(["Bahia", "Mato Grosso do Sul"])
+estados.reverse()
+
+print(estados)
