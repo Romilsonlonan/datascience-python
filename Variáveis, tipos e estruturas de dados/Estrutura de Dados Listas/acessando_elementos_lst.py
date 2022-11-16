dd79a76893d301f6c8c5a51b71a35d8d23acf838
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-16 17:51:38 -0300

    Lista com operador in

--- Variáveis, tipos e estruturas de dados/Estrutura de Dados Listas/acessando_elementos_lst.py
+++ Variáveis, tipos e estruturas de dados/Estrutura de Dados Listas/acessando_elementos_lst.py
@@ -1,9 +1,11 @@
-# A sintaxe para acessar um elemento de uma lista é a mesma usada para acessar um 
-# caractere de um string. Nós usamos o operador de indexação ( [] – não confundir 
-# com a lista vazia). A expressão dentro dos colchetes especifica o índice. Lembrar 
-# que o índice do primeiro elemento é 0. Qualquer expressão que tenha como resultado
-# um número inteiro pode ser usada como índice e como com strings, índices negativos 
-# indicarão elementos da direita para a esquerda ao invés de da esquerda para a direita.
+"""
+A sintaxe para acessar um elemento de uma lista é a mesma usada para acessar um 
+caractere de um string. Nós usamos o operador de indexação ( [] – não confundir 
+com a lista vazia). A expressão dentro dos colchetes especifica o índice. Lembrar 
+que o índice do primeiro elemento é 0. Qualquer expressão que tenha como resultado
+um número inteiro pode ser usada como índice e como com strings, índices negativos 
+indicarão elementos da direita para a esquerda ao invés de da esquerda para a direita.
+"""
 
 numeros = [17, 123, 87, 34, 66, 8398, 44]
 print(numeros[2])
--- Variáveis, tipos e estruturas de dados/Estrutura de Dados Listas/lista_op_in.py
+++ Variáveis, tipos e estruturas de dados/Estrutura de Dados Listas/lista_op_in.py
@@ -1,2 +1,4 @@
+# Lista com operador in
+
 lista_teste_op = [100, 2, -5, 3.4]
 print(10 in lista_teste_op)
