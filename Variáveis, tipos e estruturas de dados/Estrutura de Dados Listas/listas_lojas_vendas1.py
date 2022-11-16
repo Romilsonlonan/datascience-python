7385dde3a0ef5f18da9e76fbfbb0661dda8e17a6
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-16 11:56:40 -0300

    Listas de lojas e vendas

---
+++ Variáveis, tipos e estruturas de dados/Estrutura de Dados Listas/listas_lojas_vendas1.py
@@ -0,0 +1,11 @@
+# Criando uma lista com valores numéricos
+
+lojas = ['Rio de Janeiro','São Paulo', 'Curitiba']
+vendas = [10000, 20000, 50000]
+
+lojas.sort()
+vendas.sort()
+
+print(lojas)
+print(vendas)
+['Curitiba', 'Rio de Janeiro', 'São Paulo']
