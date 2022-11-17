86469d4b5c58d7c3152a9e10a4462f5211e5f7a5
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-17 12:33:26 -0300

    Imprimindo o valor que esta associado ao objeto

--- Variáveis, tipos e estruturas de dados/Dicionários/dicionario5.py
+++ Variáveis, tipos e estruturas de dados/Dicionários/dicionario5.py
@@ -1,3 +1,13 @@
+"""
+Todos os tipos de dados compostos que estudamos em detalhes até agora — strings, listas e tuplas — 
+são coleções sequenciais. Isto significa que os itens na coleção estão ordenados da esquerda para a 
+direita e eles usam números inteiros como índices para acessar os valores que eles contêm.
+
+Dicionário é um tipo diferente de coleção. Ele é um tipo de mapeamento nativo do Python. Um mapa é 
+uma coleção associativa desordenada. A associação, ou mapeamento, é feita a partir de uma chave, que 
+pode ser qualquer tipo imutável, para um valor, que pode ser qualquer objeto de dados do Python.
+"""
+
 eng2sp = {}
 eng2sp['one'] = 'uno'
 eng2sp['two'] = 'dos'
---
+++ Variáveis, tipos e estruturas de dados/Dicionários/dicionario6.py
@@ -0,0 +1,4 @@
+# Imprimindo do valor que esta associado ao objeto
+
+mydict = {"cat":12, "dog":6, "elephant":23}
+print(mydict["dog"])
