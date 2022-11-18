26b040fbc1c66cb459eced351bd38b83b780f2cc
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-18 14:14:41 -0300

    Estruturas Condicionais em python - Exercicio 4

---
+++ Loops, Condicionais, Métodos e Funções/Condicional  IF/condicional_if4.py
@@ -0,0 +1,14 @@
+"""
+if 5 = 5:
+    print("Testando Python")
+
+    # erro de sintax
+"""
+
+if 5 == 5:
+    print("Testando Python")
+
+# o resultado é boleano 
+
+if True:
+    print("Python está funcionando!!!")
