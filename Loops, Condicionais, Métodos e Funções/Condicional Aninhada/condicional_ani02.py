3f7111eb94710a3c1f40d1b9a8c54bce633aa3a8
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-18 14:49:28 -0300

    Estruturas Condicionais com Input - Exercicio 1

---
+++ Loops, Condicionais, Métodos e Funções/Condicional Aninhada/condicional_ani02.py
@@ -0,0 +1,8 @@
+print('1. vasco')
+print('2. Flamengo')      
+resposta = int (input('Qual melhor time: ') )
+
+if resposta == 1: 
+    print('Você deve ser torcedor vascaíno')
+else:             
+    print('Você deve torcer Flamenguista')
