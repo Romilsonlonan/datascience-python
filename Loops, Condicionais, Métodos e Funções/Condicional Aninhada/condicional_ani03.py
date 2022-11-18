5f1052e1111da33bba11c3f31f9e557a0ced2dc2
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-18 15:19:52 -0300

    Estruturas Condicionais Aninhados - Exercicio 3

--- Loops, Condicionais, Métodos e Funções/Condicional Aninhada/condicional_ani03.py
+++ Loops, Condicionais, Métodos e Funções/Condicional Aninhada/condicional_ani03.py
@@ -1,8 +1,14 @@
+"""
+Estruturas Codicionais Aninhadas são várias condições em cascatas, ou seja, um IF dentro de outro IF. 
+Uma outra estrutura de aninhamento é como pode-se notar abaixo. Incrementando o nosso exemplo, agora 
+teremos que exibir uma mensagem caso o valor seja igual a Zero.
+"""
+
 soma = 0
 
 if soma > 0:
      print ("Maior que Zero.")
-elif soma = 0:
+elif soma == 0:
      print ("Igual a Zero.")
 else:
      print ("Menor que Zero.")
