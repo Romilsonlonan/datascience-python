45704635eb9933b2aa44724fa59532458493dc99
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-18 14:54:20 -0300

    Estruturas Condicionais com Input - Exercicio 2

---
+++ Loops, Condicionais, Métodos e Funções/Condicional com INPUT/condicional_input02.py
@@ -0,0 +1,11 @@
+print('1. Vasco')
+print('2. Flamengo')      
+resposta = int (input('Qual melhor time: ') )
+
+if resposta == 1: 
+    print('Você deve ser vascaíno')
+else:             
+    if resposta == 2:
+        print('Você deve ser flamenguista')
+    else:
+        print('Certamente você não de ser vascaíno ou flamenguista')
