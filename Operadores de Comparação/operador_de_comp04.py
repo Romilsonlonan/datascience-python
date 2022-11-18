8bc11c4a7db570deee2c86a8ac1a170b94270242
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-18 17:09:30 -0300

    Operadores de Comparação - exercício 4

---
+++ Operadores de Comparação/operador_de_comp04.py
@@ -0,0 +1,8 @@
+dia = "segunda"
+
+if dia == "segunda":
+    print("Hoje fará sol!")
+elif dia == "terça":
+    print("Hoje vai chover!")    
+else:
+    print("Sem previsão de tempo para os dias selecionados!")
