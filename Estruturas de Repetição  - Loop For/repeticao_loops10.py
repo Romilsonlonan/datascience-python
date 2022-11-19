0ec6862e5a56420b2f5a499e084b96e1d6cbe64a
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-19 19:42:38 -0300

    Estrutura de Repetição loops aninhados - exercício 10

---
+++ Estruturas de Repetição  - Loop For/repeticao_loops10.py
@@ -0,0 +1,9 @@
+# PESQUISANDO EM LISTAS 
+
+listaC = [5, 6, 7 , 8, 10, 20 ,50, 100]
+
+# LOOP ATRAVÉS DE LISTAS 
+for item in listaC:
+    if item == 5:
+        print ('Numero encontrado na lista!')
+ 
