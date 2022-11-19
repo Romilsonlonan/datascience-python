4ca974c61193d8e024153e21b8ab10acff041163
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-19 17:17:29 -0300

    Estrutura de Repetição FOR com a função ranger em Python - exercício 2

---
+++ Estruturas de Repetição  - Loop For/repeticao_for_range02.py
@@ -0,0 +1,13 @@
+"""
+A função range()
+
+Ela permite especificar o inicio de uma sequência, o passo (ou pulo) e 
+valor final. Com isso, o Python nos retorna uma sequência de números 
+para que possamos iterar!
+"""
+
+lojas = ['Rio de Janeiro', 'São Paulo', 'Belo Horizonte', 'Curitiba']
+
+for i in range(4):
+    print (i) 
+print(lojas[1])   
