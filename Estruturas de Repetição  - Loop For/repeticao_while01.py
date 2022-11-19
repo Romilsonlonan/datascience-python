eb27a6d9f1fd611f53569d7ccda2d2417fa38842
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-19 18:19:40 -0300

    Estrutura de Repetição while com loop infinito - exercício 01

---
+++ Estruturas de Repetição  - Loop For/repeticao_while01.py
@@ -0,0 +1,9 @@
+"""
+A instrução Python while é uma das estruturas de repetição disponíveis na linguagem Python, que permite 
+executar um bloco de códigos enquanto uma determinada condição for verdadeira. Esse é um comando muito 
+utilizado pelas pessoas programadoras no desenvolvimento de aplicações.
+"""
+
+nome = input('Insira um nome ')
+while nome:
+    nome = input('Insira um nome ')
---
+++ Estruturas de Repetição  - Loop For/repeticao_whileloop1.py
@@ -0,0 +1,32 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+total_de_tentativas = 0
+
+rodada = 1
+
+while (total_de_tentativas <= 3):
+        print("Tentativa", rodada, "de", total_de_tentativas)
+
+chute_str = input("Digite o seu número: ")
+print("Você digitou: ", chute_str)
+chute = int(chute_str)
+
+acertou = numero_secreto == chute
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou):
+    print("Parabéns, você acertou")
+else:
+    if (maior):
+        print("Você digitou um número maior que o número secreto")
+    elif (menor):
+        print("Você digitou um número menor que o núnmero secreto")
+
+total_de_tentativas = total_de_tentativas +1;
+
+print("Fim do jogo")
