4357f45fdc2cb31745ef79d9eee871292bd99e16
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-20 15:19:34 -0300

    Métodos para objetos - exercício 02

---
+++ Metodos/metodos01.py
@@ -0,0 +1,10 @@
+# Criando uma lista com o método 
+lst = [100, -2, 12, 65, 0]
+
+#lst.append(10)
+lst.count(10)
+# lst(é o objeto), append(método), (10) argumento ou parâmetro
+
+print(lst)
+
+
---
+++ Metodos/metodos02.py
@@ -0,0 +1,7 @@
+a = 'Isso é uma string'
+# Variável a
+
+print(a.split())
+
+# Função(print ), dentro da função print chamamos o objeto (a) e o nome do método (sprint()) e por 
+# dentro -->(dsfdf) chamamos parâmetro. 
