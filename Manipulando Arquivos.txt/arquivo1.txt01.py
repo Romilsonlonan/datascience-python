fd3588ad7be5c9e9a85c27fc1e417c51eb72b303
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-21 17:59:18 -0300

    Manipulando arquivos em Python - Exercício 01

--- Manipulando Arquivos.txt/arquivo1.txt01.py
+++ Manipulando Arquivos.txt/arquivo1.txt01.py
@@ -1,2 +1,9 @@
+"""
+read() O método em Python é usado para ler no máximo n bytes do arquivo associado ao 
+descritor de arquivo fornecido. Se o fim do arquivo for alcançado durante a leitura 
+de bytes do descritor de arquivo fornecido, o os. read() método retornará um objeto 
+de bytes vazios para todos os bytes restantes para serem lidos.
+"""
+
 arq1 = open("arquivo1.txt", "r")
 print(arq1.read())
