1b1445fa2e3931cf49741d7dafacf7eee9ca4930
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-21 17:57:53 -0300

    Manipulando arquivos em Python - Exercício 02

--- Manipulando Arquivos.txt/arquivo1.txt02.py
+++ Manipulando Arquivos.txt/arquivo1.txt02.py
@@ -1,2 +1,8 @@
+"""
+tell() método retorna a posição atual do objeto de arquivo. Este método não aceita parâmetros 
+e retorna um valor inteiro. Inicialmente, o ponteiro do arquivo aponta para o início do arquivo 
+(se não for aberto no modo anexar). Portanto, o valor inicial de tell() é zero.
+"""
+
 arq1 = open("arquivo1.txt", "r")
 print(arq1.tell())
