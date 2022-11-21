c4f3d50cb0e80189bdda89238143e51dcca97b0a
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-21 17:56:15 -0300

    Manipulando arquivos em Python - Exercício 03

--- Manipulando Arquivos.txt/arquivo1.txt03.py
+++ Manipulando Arquivos.txt/arquivo1.txt03.py
@@ -1,2 +1,8 @@
+"""
+Em Python, a seek() função é usada para alterar a posição do identificador de arquivo 
+para uma determinada posição. O identificador de arquivo é como um cursor, que define 
+de onde os dados devem ser lidos ou gravados no arquivo.
+"""
+
 arq1 = open("arquivo1.txt", "r")
 print(arq1.seek(0,0))
