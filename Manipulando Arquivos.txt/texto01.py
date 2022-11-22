00c21ddd9ff6db3453e78b5be9f5f1b6d061c17f
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-22 00:47:07 -0300

    Manipulando texto - Exercícios 1

---
+++ Manipulando Arquivos.txt/texto01.py
@@ -0,0 +1,6 @@
+texto = "Cientista de dados é a profissão que  mais tem crescido em todos o mundo. \n"
+texto = texto + "Esses profissionais precisam se especializar em Programação, Estatística e Machine Learning. \n"
+texto += "E claro, em Big Data."
+
+print(texto)
+
