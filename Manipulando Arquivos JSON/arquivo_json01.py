385ebab72d90a70b1b7bf759492addd1928814a3
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-22 01:47:30 -0300

    Manipulando arquivos JSON - Exercícios 1

---
+++ Manipulando Arquivos JSON/arquivo_json01.py
@@ -0,0 +1,10 @@
+# Criando um dicionário 
+
+dict = {'nome': 'Romilson Luis',
+        'linguagem': 'Python',
+        'similar': ['c', 'Modula-3', 'lisp'],
+        'users': 10000000 
+        }
+
+for k,v in dict.items():
+    print(k,v)
