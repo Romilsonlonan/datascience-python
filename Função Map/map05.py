f90bbe6d5efc19080fec0f4b1b21bd42e2a25259
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-23 20:18:07 -0300

    Fução Map em Python - Exercicio 5

--- Função Map/map05.py
+++ Função Map/map05.py
@@ -1,3 +1,9 @@
+"""
+O que são expressões Lambda no Python? As funções Lambda são chamadas de funções anônimas, mas o
+que são elas? Nada mais são do que funções que o usuário não precisa definir, ou seja, não vai 
+precisar escrever a função e depois utilizá-la dentro do código.
+"""
+
 a = [1, 2, 3, 4, 5] 
 b = [6, 7, 8, 9]
 
