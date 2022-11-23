8e59c5d0917f53a57ce08e57fa56bf5cace8c52f
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-23 01:38:34 -0300

    Pacotes Datetime  em Python - Exercicio 1

---
+++ Pacote Datetime/datetime01.py
@@ -0,0 +1,11 @@
+"""
+O módulo datetime fornece as classes para manipulação de datas e horas. Ainda que a 
+aritmética de data e hora seja suportada, o foco da implementação é na extração 
+eficiente do atributo para formatação da saída e manipulação.
+"""
+
+import datetime
+
+agora = datetime.datetime.now()
+
+print(datetime.datetime.now())
