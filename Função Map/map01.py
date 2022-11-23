a3c5337aa7788c1d49c7e0c5bc076e3746f26d8b
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-23 15:56:04 -0300

    Fução Map em Python - Exercicio 1

--- Função Map/map01.py
+++ Função Map/map01.py
@@ -1,17 +1,22 @@
+f# Criando duas funções 
 from re import T
 
-
+# Função 1 --> Recebe uma temperatura como parâmentro e retorna a temperatura em Fahrenheit
 def fahrenheit(T):
     return((float(9)/5)*T + 32)
 
-
+# Função 1 --> Recebe uma temperatura como parâmentro e retorna a temperatura em Celsius
 def celsius(T):
     return ((float(9)/5)*T + 32)
 
-
+# Criando uma lista
 temperaturas = [0, 22.5, 40, 100]
 
+# Aplicando a função a cada elemento da lista de temperaturas.
+# Em python3, "a função map() retorna um interator" (<map at 0x111c28320>)
 map(fahrenheit, temperaturas)
+
+# A função map() retornando a lista de temperaturas convertidas em Fahrenheit  
 list(map(fahrenheit, temperaturas))
 
 print(list(map(fahrenheit, temperaturas)))
