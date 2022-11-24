7ed1332345be75738c4b2093cf6d65f6586608fd
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-24 13:38:12 -0300

    Excessões em Python - Exercicio 1

--- Erros e Excessões/excessoes01.py
+++ Erros e Excessões/excessoes01.py
@@ -1,4 +1,11 @@
-# Excessão
+"""
+Segundo a documentação do Python há pelo menos dois tipos distintos de erros: erros de sintaxe 
+e exceções. Os erros de sintaxe apontam para a linha de nosso script e, normalmente são fáceis 
+de se corrigir, veja um exemplo abaixo. Neste erro eu esqueço de iniciar a string com o sinal 
+de aspas ". Uma exceção é um objeto da classe Exception, ou de uma de suas subclasses. Ele 
+permite armazenar informações sobre situações excepcionais que venham a ocorrer durante a 
+execução de comandos dentro de algum método.
+"""
 
 def numDiv (num1, num2):
     resultado = num1 / num2
