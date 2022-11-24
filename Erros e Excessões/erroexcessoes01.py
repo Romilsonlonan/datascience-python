3e1febfbf45265a224195dbb99794b062683748a
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-24 12:58:05 -0300

    Erros e Excessões em Python - Exercicio 1

--- Erros e Excessões/erroexcessoes01.py
+++ Erros e Excessões/erroexcessoes01.py
@@ -1,3 +1,13 @@
-# Erro
+# Erro de aspas
 
-print('Olá)
+# print('Olá)
+
+"""
+File "/home/romilson/Projetos e Treinamentos/DataScience-Python/Erros e Excessões/erroexcessoes01.py", line 3
+    print('Olá)
+          ^
+"""
+
+# correto
+
+print('Olá')
