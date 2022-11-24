6433cb893226845dd57d963a8ae72c1a5a6e04d7
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-24 03:00:13 -0300

    Fução List Comprehensin em Python - Exercicio 8

---
+++ List Comprehension/listcomprehension08.py
@@ -0,0 +1,8 @@
+# Lista
+lst = [100,45,294,23904,395,3864,87495,233,2245,67,788,998,776,900]
+# LC sintaxe
+[x for x in lst if x < 300]
+
+# If - Else
+print([x if x<300 else "Maior" for x in lst])
+
