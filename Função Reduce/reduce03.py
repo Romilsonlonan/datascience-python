314ecd1345fe87f7b9160386588599bfdbcc454a
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-23 22:59:23 -0300

    Fução Reduce em Python - Exercicio 3

--- Função Reduce/reduce03.py
+++ Função Reduce/reduce03.py
@@ -2,8 +2,10 @@ from functools import reduce
 
 lst = [47, 11, 42, 13] 
 
+# Podemos atribuir a expressão lambda a uma variável  
 max_find2 = lambda a,b: a if(a > b) else b
 
 type (max_find2)
 
+# reduzindo a lista até o valor máximo, através da função criada com a expressão lambda   
 print(reduce(max_find2, lst))
