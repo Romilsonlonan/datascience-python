80250057892b5995650b9f9a8660d8dca99c089a
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-24 11:27:02 -0300

    Fução Zip em Python - Exercicio 8

---
+++ Zip e Enumerate/zip08.py
@@ -0,0 +1,10 @@
+"""
+Lista de tamanhos diferentes serão equiparadas e 
+a diferença entre elas será desconsiderado.
+"""
+
+list_a = [6, 7, 8, 9]
+list_b = [1, 2, 3, 4, 5]
+
+for x in zip(list_a, list_b):
+    print(x)
