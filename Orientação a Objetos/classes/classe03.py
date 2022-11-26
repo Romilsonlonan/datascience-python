5af7eb99f2bca67855c72daa1e36a99d392aab6c
Romilsonlonan <romilsonlonan@gmail.com>
2022-11-25 21:15:25 -0300

    Criando Objetos em Python - exercicio 1

--- Orientação a Objetos/classes/classe03.py
+++ Orientação a Objetos/classes/classe03.py
@@ -15,10 +15,22 @@ print(Func1.listFunc())
 
 print("****Usando atributos****")
 
+# O objete func1 tem atributo nome?
 print(hasattr(Func1, "nome"))
 
+# O objeto func1 tem atributo salário?
 print(hasattr(Func1, "salario"))
 
+# a partir do objeto func1 verifique o salário e defina a ele o novo valor de atributo
 print(setattr(Func1, "salario", 4500))
 
-print(hasattr(Func1, "salario"))
+# obtenha esse atributo a partir do objeto 
+print(getattr(Func1, "salario"))
+
+# delete o objeto 
+print(delattr(Func1, "salario"))
+
+# verifique ou não se exite o objeto
+print(hasattr(Func1, "nome"))
+
+
